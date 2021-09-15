589799cbcf32d9f8a10d08e13e6c1f0156728a84
Qalandar Axmedov <64014854+QalandarAxmedov@users.noreply.github.com>
2021-09-15 03:28:09 -0400

    Create id0108.cs

---
+++ Tasks for beginners/id0108.cs
@@ -0,0 +1,12 @@
+using System;
+ 
+namespace task0108
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(Console.ReadLine());
+        }
+    }
+}
