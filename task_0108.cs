4b21c69d7f5a430d0f8279ff7c0f77b0097c0de3
Qalandar Axmedov <64014854+QalandarAxmedov@users.noreply.github.com>
2021-09-15 03:25:59 -0400

    Create task_0108.cs

---
+++ task_0108.cs
@@ -0,0 +1,12 @@
+using System;
+ 
+namespace task_0108
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(Console.ReadLine());
+        }
+    }
+}
