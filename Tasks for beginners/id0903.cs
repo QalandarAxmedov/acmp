8962a99b4af9d2f3123ba9b60b25bfe7236c2ba2
Qalandar Axmedov <64014854+QalandarAxmedov@users.noreply.github.com>
2021-09-15 03:30:59 -0400

    Create id0903.cs

---
+++ Tasks for beginners/id0903.cs
@@ -0,0 +1,12 @@
+using System;
+ 
+namespace Task0903
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(Convert.ToInt32(Console.ReadLine())+1);
+        }
+    }
+}
